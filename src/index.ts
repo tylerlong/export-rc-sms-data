2c098f99fdbc73d4e80cd1b5496e38b514776c5d
Tyler Long <tyler4long@gmail.com>
2020-09-19 10:36:00 -0700

    First fully working version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import RingCentral from '@rc-ex/core';
 import RateLimitExtension from '@rc-ex/rate-limit';
-import {CompanyPhoneNumberInfo} from '@rc-ex/core/lib/definitions';
+import {
+  CompanyPhoneNumberInfo,
+  GetMessageInfoResponse,
+} from '@rc-ex/core/lib/definitions';
+import MessageStore from '@rc-ex/core/lib/paths/Restapi/Account/Extension/MessageStore';
 import papaparse from 'papaparse';
 import fs from 'fs';
 import path from 'path';
@@ -27,6 +31,28 @@ const result: {
   lastModifiedTime: string;
 }[] = [];
 
+const date = new Date();
+date.setDate(date.getDate() - 10); // 10 days ago
+const messageStorePagination = async (messageStore: MessageStore) => {
+  let result: GetMessageInfoResponse[] = [];
+  let page = 1;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const r = await messageStore.list({
+      messageType: ['SMS'],
+      perPage: 1000,
+      dateFrom: date.toISOString(),
+      page,
+    });
+    result = [...result, ...r.records!];
+    if (r.navigation?.nextPage === undefined) {
+      break;
+    }
+    page += 1;
+  }
+  return result;
+};
+
 (async () => {
   // find all matching numbers in this account
   let page = 1;
@@ -47,13 +73,17 @@ const result: {
 
   // fetch SMS
   for (const phoneNumber of phoneNumbers) {
-    const r = await rc
-      .restapi()
-      .account()
-      .extension(phoneNumber.extension!.id!)
-      .messageStore()
-      .list({messageType: ['SMS']});
-    for (const record of r.records!) {
+    console.log(`Fetch sms for ext ${phoneNumber.extension?.extensionNumber}`);
+    const records = await messageStorePagination(
+      rc
+        .restapi()
+        .account()
+        .extension(phoneNumber.extension!.id!)
+        .messageStore()
+    );
+
+    // convert to CSV format
+    for (const record of records) {
       result.push({
         id: record.id!.toString(),
         direction: record.direction!,
@@ -70,6 +100,7 @@ const result: {
     }
   }
 
+  // save the CSV file
   const csvString = papaparse.unparse(result);
   fs.writeFileSync(path.join(__dirname, 'result.csv'), csvString);
 })();
