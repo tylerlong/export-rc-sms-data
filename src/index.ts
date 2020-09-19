fa08642ad9fb828fd637114768ae2d6ec23b87dd
Tyler Long <tyler4long@gmail.com>
2020-09-18 19:06:51 -0700

    Hello RingCentral

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL!,
+});
+rc.token = {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN};
+
+(async () => {
+  const ext = await rc.restapi().account().extension().get();
+  console.log(ext);
+})();
